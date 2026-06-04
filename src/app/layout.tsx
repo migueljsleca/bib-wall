13c1c7be8c492281391b9ce7da641abf0030760b
migueljsleca <migueljsleca@gmail.com>
2026-06-04 16:17:49 +0100

    Add Cloudflare Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <Script
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token":"2ba5f03fbc214f3bb3e36f6db3aa7d66"}'
+      />
     </html>
   );
 }
